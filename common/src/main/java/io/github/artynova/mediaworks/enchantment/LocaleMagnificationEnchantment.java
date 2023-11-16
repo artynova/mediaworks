82bb4a9f13244a7ed619b77e93363cee8ffbf611
Anna <annasonohana@gmail.com>
2023-11-16 01:42:42 -0500

    adjusted formula to allow for level extension of Locale Magnification

--- common/src/main/java/io/github/artynova/mediaworks/enchantment/LocaleMagnificationEnchantment.java
+++ common/src/main/java/io/github/artynova/mediaworks/enchantment/LocaleMagnificationEnchantment.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.player.PlayerEntity;
 
 public class LocaleMagnificationEnchantment extends CloakEnchantment {
     public static final int MAX_LEVEL = 3;
-    public static final int[] BONUSES = new int[]{8, 16, 32};
 
     public LocaleMagnificationEnchantment() {
         super(Rarity.VERY_RARE);
@@ -16,7 +15,7 @@ public class LocaleMagnificationEnchantment extends CloakEnchantment {
     public static int getAmbitIncrease(PlayerEntity player) {
         int level = Math.min(MAX_LEVEL, EnchantmentHelper.getLevel(MediaworksEnchantments.LOCALE_MAGNIFICATION.get(), player.getEquippedStack(EquipmentSlot.HEAD)));
         if (level <= 0) return 0;
-        return BONUSES[level - 1];
+        return 8*level+Math.max(0,8*level-16);
     }
 
     @Override
